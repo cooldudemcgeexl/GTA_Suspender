aa82ad28f2982534326e011ef2dbaa1625c2c98b
Nick Lay <layns@mail.uc.edu>
2020-05-16 00:15:25 -0400

    Added icon, will improve later

--- GTA_Suspender/Form1.Designer.cs
+++ GTA_Suspender/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
@@ -86,6 +87,7 @@
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "GTA V Suspender";
             this.Load += new System.EventHandler(this.Form1_Load);
